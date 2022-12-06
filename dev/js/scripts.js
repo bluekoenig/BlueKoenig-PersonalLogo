81a7b11dabcdfb94494d1ea6f4d1d08c912a266d
Blue <bk647@drexel.edu>
2022-12-06 10:28:58 -0500

    some form of pegres

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -3,10 +3,9 @@ import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
 import { GSDevTools } from "gsap/GSDevTools";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
 
-gsap.registerPlugin(DrawSVGPlugin, GSDevTools);
-gsap.registerPlugin(MotionPathPlugin, GSDevTools);
-gsap.registerPlugin(ScrollTrigger, GSDevTools);
+gsap.registerPlugin(ScrollTrigger, MotionPathPlugin, DrawSVGPlugin, MorphSVGPlugin);
 
 
 
@@ -15,7 +14,7 @@ function fishonaMish(){
   gsap.to("#the-fishie", {
     duration: 15, 
     repeat: 12,
-    repeatDelay: 3,
+    repeatDelay: 3
 
     
   });
@@ -41,37 +40,60 @@ function fishonaMish(){
           alignOrigin: [0.5, 0.5],
           start: .188,
           end: .45
-        }, scale:3, morphSVG:"#word-fish"})
-        tl.to("#the-fishie",{
-          motionPath:{
-            path: "#fishpath",
-            align: "#fishpath",
-            autoRotate: true,
-            alignOrigin: [0.5, 0.5],
-            start: .45,
-            end: .75,
-            duration: 1
-          }, scale: 1})
-          tl.to("#the-fishie",{
-            motionPath:{
-              path: "#fishpath",
-              align: "#fishpath",
-              autoRotate: true,
-              alignOrigin: [0.5, 0.5],
-              start: .75,
-              end: 1,
-              duration: 1
-            }})
+        }, scale:3})
+        // tl.to("#the-fishie",{
+        //   motionPath:{
+        //     path: "#fishpath",
+        //     align: "#fishpath",
+        //     autoRotate: true,
+        //     alignOrigin: [0.5, 0.5],
+        //     start: .45,
+        //     end: .75,
+        //     duration: 1
+        //   }, scale: 1})
+        //   tl.to("#the-fishie",{
+        //     motionPath:{
+        //       path: "#fishpath",
+        //       align: "#fishpath",
+        //       autoRotate: true,
+        //       alignOrigin: [0.5, 0.5],
+        //       start: .75,
+        //       end: 1,
+        //       duration: 1
+        //     }})
         
-            gsap.to("#the-fishie", {duration: 1, morphSVG:"#hippo"});
+            tl.to("#the-fishie", {morphSVG:".fisk", duration:2}, "+=1");
 
+            tl.to("word-fish",{
+        
+              motionPath:{
+                stagger:10,
+                path: "#fishpath",
+                align: "#fishpath",
+                autoRotate: true,
+                alignOrigin: [0.5, 0.5],
+                start: .188,
+                end: .45
+              }, scale:3})
+              tl.to("#word-fish",{
+                motionPath:{
+                  path: "#fishpath",
+                  align: "#fishpath",
+                  autoRotate: true,
+                  alignOrigin: [0.5, 0.5],
+                  start: .45,
+                  end: .75,
+                  duration: 1
+                }, scale: 1})
     return tl;
 
 }
 
-function patternMotion(){
+function plopPlop(){
 
-    var tl = gsap.timeline();
+    var tl = gsap.timeline({repeat:3});
+
+    gsap.fromTo(".firstDown",{duration:2, DrawSVGPlugin:"100%"});
 
     tl.from(".odd",{duration:2, drawSVG:0, stagger:0.25, rotate:180, transformOrigin:"center"},"spin")
     .fromTo(".even",{drawSVG:"0% 0%"},{duration:2, drawSVG:"0% -100%", stagger:0.25, rotate:180, transformOrigin:"center"},"spin");
@@ -82,30 +104,13 @@ function patternMotion(){
 function UIMotion(){
 
 
-    // make a var that represents the line in the SVG
-    var bottomLine = document.querySelector("#bottom-line");
-    // make a var that uses the svg line var from above and sets the BBox property
-    bottomLine = bottomLine.getBBox();
-
-    console.log(bottomLine.width);
-
-    var tl = gsap.timeline();
-    tl.to("#pencil",{duration:0.5,x:bottomLine.width},"draw")
-    .from("#bottom-line",{duration:0.5,drawSVG:0},"draw")
-    .to("#pencil",{duration:0.25, alpha:0},"drawOutline")
-    .fromTo("#outline",{drawSVG:"100% 100%"},{duration:0.5, drawSVG:"100% 0%"},"drawOutline")
-    .from("#box-bg",{duration:0.5, scaleX:0})
-    .from(".letter",{duration:0.25, alpha:0, y:20, stagger: 0.12},"-=50%")
-    .from("#glass",{duration:0.25, drawSVG:0},"-=.5")
-    .from("#handle",{duration:0.25, drawSVG:0},"-=.25")
-    return tl;
 
 }
 
 
 var mainTL = gsap.timeline();
 mainTL.add(fishonaMish())
-.add(patternMotion())
+.add(plopPlop())
 .add(UIMotion())
 
 
