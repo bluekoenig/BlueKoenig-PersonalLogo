a95427bbdb05b6d7c9a13993789c5b978aeb3b79
Blue <bk647@drexel.edu>
2022-12-05 23:33:03 -0500

    its jumping!!!!!

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -5,13 +5,15 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { MotionPathPlugin } from "gsap/MotionPathPlugin";
 
 gsap.registerPlugin(DrawSVGPlugin, GSDevTools);
-gsap.registerPlugin(MotionPathPlugin, GSDevTools)
-gsap.registerPlugin(ScrollTrigger, GSDevTools)
+gsap.registerPlugin(MotionPathPlugin, GSDevTools);
+gsap.registerPlugin(ScrollTrigger, GSDevTools);
+
+
 
 function fishonaMish(){
 
   gsap.to("#the-fishie", {
-    duration: 5, 
+    duration: 15, 
     repeat: 12,
     repeatDelay: 3,
 
@@ -26,9 +28,41 @@ function fishonaMish(){
         autoRotate: true,
         alignOrigin: [0.5, 0.5],
         start: 0,
-        end: .17,
-        stagger: .5
+        end: .188,
+        duration: 1
       }})
+      tl.to("#the-fishie",{
+        
+        motionPath:{
+          stagger:10,
+          path: "#fishpath",
+          align: "#fishpath",
+          autoRotate: true,
+          alignOrigin: [0.5, 0.5],
+          start: .188,
+          end: .45
+        }, scale:3}, "bigGuy")
+        tl.to("#the-fishie",{
+          motionPath:{
+            path: "#fishpath",
+            align: "#fishpath",
+            autoRotate: true,
+            alignOrigin: [0.5, 0.5],
+            start: .45,
+            end: .75,
+            duration: 1
+          }, scale: 1})
+          tl.to("#the-fishie",{
+            motionPath:{
+              path: "#fishpath",
+              align: "#fishpath",
+              autoRotate: true,
+              alignOrigin: [0.5, 0.5],
+              start: .75,
+              end: 1,
+              duration: 1
+            }})
+        
      
 
     return tl;
