9b634557d3536b1df250ad578e1321318037aeb4
Blue <bk647@drexel.edu>
2022-12-08 08:41:12 -0500

    restarted again now its working better

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,108 +1,80 @@
 import { gsap } from "gsap";
-import {DrawSVGPlugin} from "gsap/DrawSVGPlugin"
-import {MotionPathPlugin} from "gsap/MotionPathPlugin"
-import {MorphSVGPlugin} from "gsap/MorphSVGPlugin"
-import {GSDevTools} from "gsap/GSDevTools"
+import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { GSDevTools } from "gsap/GSDevTools";
+import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
 
-gsap.registerPlugin(MotionPathPlugin, DrawSVGPlugin, MorphSVGPlugin);
-gsap.registerPlugin(DrawSVGPlugin);
+gsap.registerPlugin(MotionPathPlugin, DrawSVGPlugin, GSDevTools, MorphSVGPlugin);
+
+// function lineBreaker(){
+// var tl = gsap.timeline();
+// tl.to("#secondRightSplash", {drawSVG: "0% 0%"})
+//     .to("#downLeft", {drawSVG:"0% 0%"})
+//     .to("#secondLeftSplash", {drawSVG:"0% 0%"})
+//     .to("#upLeft", {drawSVG:"0% 0%"})
+//     return tl; 
+// }
 
 
-function fishonaMish(){
-
-    var tl = gsap.timeline();
-    tl.to("#the-fishie",{
-      motionPath:{
-        path: "#fishpath",
-        align: "#fishpath",
-        autoRotate: true,
-        alignOrigin: [0.5, 0.5],
-        start: 0,
-        end: .188,
-        duration: 1
-      }})
-      tl.to("#the-fishie",{
-        
-        motionPath:{
-          stagger:10,
-          path: "#fishpath",
-          align: "#fishpath",
-          autoRotate: true,
-          alignOrigin: [0.5, 0.5],
-          start: .188,
-          end: .45
-        }, scale:3})
-        // tl.to("#the-fishie",{
-        //   motionPath:{
-        //     path: "#fishpath",
-        //     align: "#fishpath",
-        //     autoRotate: true,
-        //     alignOrigin: [0.5, 0.5],
-        //     start: .45,
-        //     end: .75,
-        //     duration: 1
-        //   }, scale: 1})
-        //   tl.to("#the-fishie",{
-        //     motionPath:{
-        //       path: "#fishpath",
-        //       align: "#fishpath",
-        //       autoRotate: true,
-        //       alignOrigin: [0.5, 0.5],
-        //       start: .75,
-        //       end: 1,
-        //       duration: 1
-        //     }})
-        
-            tl.to("#the-fishie", {morphSVG:".fisk", duration:2}, "+=1");
-
-            tl.to("#word-fish",{
-        
-              motionPath:{
-                stagger:10,
-                path: "#fishpath",
-                align: "#fishpath",
-                autoRotate: true,
-                alignOrigin: [0.5, 0.5],
-                start: .188,
-                end: .45
-              }, scale:3})
-              tl.to("#word-fish",{
-                motionPath:{
-                  path: "#fishpath",
-                  align: "#fishpath",
-                  autoRotate: true,
-                  start: .45,
-                  end: .75,
-                  duration: 1
-                }, scale: 1})
+function fishIn(){
+  gsap.set("#bareAssFish", {scaleY:-1, scaleX:-1})
+  var tl = gsap.timeline();
+    tl.to("#bareAssFish", {duration:2, motionPath:{path:"#fishPath", align:"#fishPath", alignOrigin:[0.5,0.5], autoRotate:true, start:0, end:0.44}})
     return tl;
-
 }
 
-function plopPlop(){
-
-    var tl = gsap.timeline();
-
-    tl.from("#top-line",{drawSVG:"100%"},"-=50")
-    tl.from("#middle-line",{drawSVG:"100%"}, "-=50")
-
-   
-    return tl;
-
+function fishLinesRight(){
+  var tl = gsap.timeline();
+    tl.fromTo("#downRight", {duration:0.15, drawSVG: "0% 0%"}, {drawSVG:"0% 100%"})
+    .fromTo("#downRight", {duration:0.15, drawSVG: "0% 100%"}, {drawSVG:"100% 100%"}, "<50%")
+    .fromTo("#firstRightSplash", {duration:0.05, drawSVG: "0% 0%"}, {drawSVG:"0% 100%"}, "<75%")
+    .fromTo("#firstRightSplash", {duration:0.05, drawSVG: "0% 100%"}, {drawSVG:"100% 100%"}, "<50%")
+    .fromTo("#firstLeftSplash", {duration:0.05, drawSVG: "0% 0%"}, {drawSVG:"0% 100%"}, "<0%")
+    .fromTo("#firstLeftSplash", {duration:0.05, drawSVG: "0% 100%"}, {drawSVG:"100% 100%"}, "<50%")
+    .fromTo("#upRight", {duration:0.15, drawSVG: "0% 0%"}, {drawSVG:"0% 100%"}, "<0%")
+    .fromTo("#upRight", {duration:0.15, drawSVG: "0% 100%"}, {drawSVG:"100% 100%"},"<50%")
+  return tl;
 }
 
-function UIMotion(){
+function fishMorph(){
+  var tl = gsap.timeline();
+  tl.to("#bStart", {duration:0.5, morphSVG:"#b", fill:"#00ADD4"})
+  .to("#lStart", {duration:0.5, morphSVG:"#l", fill:"#00ADD4"}, "<25%")
+  .to("#frontGill", {duration:0.5, morphSVG:"#lSide", fill:"#0F6889"}, "<25%")
+  .to("#backGill", {duration:0.5, morphSVG:"#u2", fill:"#00ADD4"}, "<25%")
+  .to("#fin", {duration:0.5, morphSVG:"#eTop", fill:"#0F6889"}, "<25%")
+  .to("#eStart", {duration:0.5, morphSVG:"#e", fill:"#00ADD4"}, "<25%")
+  .to("#tStart", {duration:0.5, morphSVG:"#t", fill:"#00ADD4"}, "<25%")
+  .to("#uStart", {duration:0.5, morphSVG:"#u", fill:"#00ADD4"}, "<25%")
+  .to("#nStart", {duration:0.5, morphSVG:"#n", fill:"#00ADD4"}, "<25%")
+  .to("#tail", {duration:0.5, morphSVG:"#a", fill:"#00ADD4"}, "<25%")
+  return tl;
+}
 
+function fishOut(){
+  var tl = gsap.timeline();
+  tl.to("#bareAssFish", {duration:2.25, motionPath:{path:"#fishPath", align:"#fishPath", offsetX:-165, offsetY:-150, autoRotate:true, start:0.44, end:1}})
+  return tl;
+}
 
-
+function fishLinesLeft(){
+  var tl = gsap.timeline();
+    tl.fromTo("#downLeft", {duration:0.15, drawSVG: "0% 0%"}, {drawSVG:"0% 100%"})
+    .fromTo("#downLeft", {duration:0.15, drawSVG: "0% 100%"}, {drawSVG:"100% 100%"}, "<50%")
+    .fromTo("#secondRightSplash", {duration:0.05, drawSVG: "0% 0%"}, {drawSVG:"0% 100%"}, "<75%")
+    .fromTo("#secondRightSplash", {duration:0.05, drawSVG: "0% 100%"}, {drawSVG:"100% 100%"}, "<50%")
+    .fromTo("#secondLeftSplash", {duration:0.05, drawSVG: "0% 0%"}, {drawSVG:"0% 100%"}, "<0%")
+    .fromTo("#secondLeftSplash", {duration:0.05, drawSVG: "0% 100%"}, {drawSVG:"100% 100%"}, "<50%")
+    .fromTo("#upLeft", {duration:0.15, drawSVG: "0% 0%"}, {drawSVG:"0% 100%"}, "<0%")
+    .fromTo("#upLeft", {duration:0.15, drawSVG: "0% 100%"}, {drawSVG:"100% 100%"},"<50%")
+  return tl;
 }
 
 
 var mainTL = gsap.timeline();
-mainTL.add(fishonaMish())
-.add(plopPlop())
-.add(UIMotion())
-
-
-
-GSDevTools.create();
+// mainTL.add(lineBreaker())
+mainTL.add(fishIn())
+mainTL.add(fishLinesRight(), "<5%")
+mainTL.add(fishMorph())
+mainTL.add(fishOut())
+mainTL.add(fishLinesLeft(), "<5%")
