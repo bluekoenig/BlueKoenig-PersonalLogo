a27d067279de3f34a1938159d8cb40795402361b
Blue <bk647@drexel.edu>
2022-12-07 21:18:08 -0500

    shit is so busted

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -5,7 +5,29 @@ import {MorphSVGPlugin} from "gsap/MorphSVGPlugin"
 import {GSDevTools} from "gsap/GSDevTools"
 
 gsap.registerPlugin(MotionPathPlugin, DrawSVGPlugin, MorphSVGPlugin);
-gsap.registerPlugin(DrawSVGPlugin);
+gsap.registerPlugin(GSDevTools);
 
 
-GSDevTools.create();
+function lafet(){
+
+  var tl = gsap.timeline();
+tl.from("#l", {motionPath: {path: "#l-path", align: "l-path", start: 0, end: .2}})
+tl.from("#a", {motionPath: {path: "#a-path", align: "a-path", start: 0, end: .2}})
+tl.from("#f", {motionPath: {path: "#f-path", align: "f-path", start: 0, end: .2}})
+tl.from("#a2", {motionPath: {path: "#a2-path", align: "a2-path", start: 0, end: .2}})
+tl.from("#y", {motionPath: {path: "#y-path", align: "y-path", start: 0, end: .2}})
+tl.from("#e", {motionPath: {path: "#e-path", align: "e-path", start: 0, end: .2}})
+tl.from("#t", {motionPath: {path: "#t-path", align: "t-path", start: 0, end: .2}})
+tl.from("#t2", {motionPath: {path: "#t2-path", align: "t2-path", start: 0, end: .2}})
+tl.from("#e2", {motionPath: {path: "#e2-path", align: "e2-path", start: 0, end: .2}})
+}
+
+function anticips(){
+
+}
+
+
+
+var mainTL = gsap.timeline();
+mainTL.add(lafet())
+.add(anticips())
